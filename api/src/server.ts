4d19409d4b9f3e3102a2a20f8f9d398503b7b4d8
Wellington Reis <wellington.costa.97@gmail.com>
2023-09-14 14:52:46 -0300

    Beta

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,23 +1,23 @@
 import { fastify } from "fastify";
-import { fastifyCors } from "@fastify/cors";
+import { fastifyCors } from '@fastify/cors'
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
-import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 
-const app = fastify();
+const app = fastify()
 
 app.register(fastifyCors, {
   origin: '*'
 })
 
-app.register(getAllPromptsRoute);
-app.register(uploadVideoRoute);
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
-app.register(generateAICompletionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
-  port: 3000,
+  port: 3333
 }).then(() => {
-  console.log('Server is running!');
+  console.log('HTTP Server Running!')
 })

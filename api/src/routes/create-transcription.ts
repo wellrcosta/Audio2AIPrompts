cba1cd4fa5c83f6849bd7e8c80bb28f7fcc551f6
Wellington Reis <wellington.costa.97@gmail.com>
2023-09-13 23:00:43 -0300

    Added openai

--- api/src/routes/create-transcription.ts
+++ api/src/routes/create-transcription.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
+import { createReadStream } from "fs";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance){
   app.post('/videos/:videoId/transcription', async (request) => {
@@ -16,10 +18,28 @@ export async function createTranscriptionRoute(app: FastifyInstance){
 
     const { prompt } = bodySchema.parse(request.body)
 
-    return {
-      videoId,
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      }
+    })
+
+    const videoPath = video.path
+
+    const audioReadStream = createReadStream(videoPath)
+
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: 'whisper-1',
+      language: 'pt-BR',
+      response_format: 'json',
+      temperature: 0.3,
       prompt,
-    }
+    })
+
+
+
+    return response.text
 
   })
 }

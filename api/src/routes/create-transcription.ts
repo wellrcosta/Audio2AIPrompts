7106546dc59e4a8d1882df2d50da025e43a2e4b9
Wellington Reis <wellington.costa.97@gmail.com>
2023-09-13 22:49:22 -0300

    Added some routes and logic about uploading video and prompts

---
+++ api/src/routes/create-transcription.ts
@@ -0,0 +1,25 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function createTranscriptionRoute(app: FastifyInstance){
+  app.post('/videos/:videoId/transcription', async (request) => {
+    const paramsSchema = z.object({
+      videoId: z.string().uuid()
+    })
+    
+    const { videoId } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      prompt: z.string(),
+    })
+
+    const { prompt } = bodySchema.parse(request.body)
+
+    return {
+      videoId,
+      prompt,
+    }
+
+  })
+}
